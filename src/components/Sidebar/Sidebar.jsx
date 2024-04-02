59be1840f23078121f6dfb35f4b08d53781cd0eb
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-02 12:12:40 +0530

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,49 @@
 import React from "react";
-import { assets } from "../../assets/assets";
+import { Settings } from "lucide-react";
 
-const Sidebar = () => {
+export function Sidebar() {
 	return (
-		<div className="">
-			{/* sidebar top */}
-			<div className="">
-				<img className="" src={assets.menu_icon} alt="" />
-				<div className="">
-					<img src={assets.plus_icon} alt="" />
-				</div>
-				<p>New Chat</p>
-			</div>
-			<div className="">
-				<p>Recent</p>
-			</div>
-			<div className="">
-				<img src={assets.message_icon} alt="" />
-				<p>What is react...</p>
-			</div>
-			{/* sidebar bottom */}
-			<div className="">
-				<div className="">
-					<img src={assets.question_icon} alt="" />
-					<p>Help</p>
-				</div>
-				<div className="">
-					<img src={assets.history_icon} alt="" />
-					<p>Activity</p>
-				</div>
-				<div className="">
-					<img src={assets.setting_icon} alt="" />
-					<p>Setting</p>
-				</div>
-			</div>
-		</div>
-	);
-};
+		<aside className="flex flex-col items-center w-16 h-screen py-8 overflow-y-auto bg-white border-r">
+			<nav className="flex flex-col items-center flex-1 space-y-6">
+				<a href="#">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="20"
+						width="20"
+						viewBox="0 0 448 512"
+					>
+						<path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+					</svg>
+				</a>
 
-export default Sidebar;
+				<a href="#">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="20"
+						width="20"
+						viewBox="0 0 448 512"
+					>
+						<path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+					</svg>
+				</a>
+			</nav>
+
+			<div className="flex flex-col items-center space-y-6">
+				<a
+					href="#"
+					className="rounded-lg bg-gray-100 p-1.5 text-gray-700 transition-colors duration-200 focus:outline-none"
+				>
+					<Settings size={24} />
+				</a>
+
+				<a href="#">
+					<img
+						className="object-cover w-8 h-8 rounded-full"
+						src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+						alt="User avatar"
+					/>
+				</a>
+			</div>
+		</aside>
+	);
+}
